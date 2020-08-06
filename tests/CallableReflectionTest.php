281fa9d5aa61ad3255ec459f8e2a13d0b50f49aa
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-08-06 13:03:02 +0000

    Add CallableReflection class with test

---
+++ tests/CallableReflectionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP Invoker.
+ *
+ * PHP version 7.1 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DivineNii\Invoker\Tests;
+
+use DivineNii\Invoker\CallableReflection;
+use DivineNii\Invoker\Exceptions\NotCallableException;
+use DivineNii\Invoker\Tests\Fixtures\BlankClass;
+use DivineNii\Invoker\Tests\Fixtures\BlankClassMagic;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use ReflectionFunction;
+use ReflectionMethod;
+
+/**
+ * CallableReflectionTest
+ */
+class CallableReflectionTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $this->assertInstanceOf(
+            ReflectionMethod::class,
+            CallableReflection::create('DivineNii\Invoker\Tests\Fixtures\BlankClass::staticMethod')
+        );
+
+        $this->assertInstanceOf(
+            ReflectionMethod::class,
+            CallableReflection::create([new BlankClass(), 'method'])
+        );
+
+        $this->assertInstanceOf(
+            ReflectionFunction::class,
+            CallableReflection::create(function (string $test): string {
+                return $test;
+            })
+        );
+
+        $this->assertInstanceOf(ReflectionFunction::class, CallableReflection::create('phpinfo'));
+    }
+
+    public function testCreateReflectionException(): void
+    {
+        $this->expectExceptionMessage(
+            'Method DivineNii\Invoker\Tests\Fixtures\BlankClassMagic::method() does not exist'
+        );
+        $this->expectException(ReflectionException::class);
+
+        CallableReflection::create([new BlankClassMagic(), 'method']);
+    }
+
+    public function testCreateNotCallableException(): void
+    {
+        $this->expectExceptionMessage('handler is not a callable');
+        $this->expectException(NotCallableException::class);
+
+        CallableReflection::create('handler');
+    }
+}
