e67cbaf5f385653f9372bd9b5fffbf2c606e30a6
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-08-06 13:15:52 +0000

    Add CallableResolver class with test

---
+++ src/CallableResolver.php
@@ -0,0 +1,176 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP Invoker.
+ *
+ * PHP version 7.1 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DivineNii\Invoker;
+
+use Closure;
+use DivineNii\Invoker\Exceptions\NotCallableException;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use ReflectionClass;
+use ReflectionMethod;
+use Throwable;
+
+/**
+ * Resolves a callable from a container.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ * @author Divine Niiquaye Ibok <divineibok@gmail.com>
+ */
+class CallableResolver
+{
+    public const CALLABLE_PATTERN = '!^([^\:]+)(:|@)([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
+
+    /**
+     * @var null|ContainerInterface
+     */
+    private $container;
+
+    public function __construct(?ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Resolve the given callable into a real PHP callable.
+     *
+     * @param array<mixed,string>|callable|string $callable
+     *
+     * @throws NotCallableException
+     *
+     * @return callable real PHP callable
+     */
+    public function resolve($callable)
+    {
+        // Shortcut for a very common use case
+        if ($callable instanceof Closure) {
+            return $callable;
+        }
+
+        if (\is_string($callable) && 1 === \preg_match(self::CALLABLE_PATTERN, $callable, $matches)) {
+            // check for callable as "class:method", and "class@method"
+            $callable = [$matches[1], $matches[3]];
+        }
+
+        if ((\is_string($callable) || \is_object($callable)) && \method_exists($callable, '__invoke')) {
+            $callable = [$callable, '__invoke'];
+        }
+
+        $callable = $this->resolveFromContainer($callable);
+
+        if (!\is_callable($callable)) {
+            throw NotCallableException::fromInvalidCallable($callable, true);
+        }
+
+        return $callable;
+    }
+
+    /**
+     * @param array<mixed,string>|callable|string $callable
+     *
+     * @throws NotCallableException
+     *
+     * @return callable
+     */
+    private function resolveFromContainer($callable)
+    {
+        $isStaticCallToNonStaticMethod = false;
+
+        // If it's already a callable there is nothing to do
+        if (\is_callable($callable)) {
+            $isStaticCallToNonStaticMethod = $this->isStaticCallToNonStaticMethod($callable);
+
+            if (!$isStaticCallToNonStaticMethod) {
+                return $callable;
+            }
+        }
+
+        // The callable is a container entry name
+        if (\is_string($callable) && null !== $this->container) {
+            try {
+                return $this->resolve($this->container->get($callable));
+            } catch (NotFoundExceptionInterface $e) {
+                if ($this->container->has($callable)) {
+                    throw $e;
+                }
+
+                throw NotCallableException::fromInvalidCallable($callable, true);
+            }
+        }
+
+        // The callable is an array whose first item is a container entry name
+        // e.g. ['some-container-entry', 'methodToCall']
+        if (\is_array($callable) && \is_string($callable[0])) {
+            try {
+                if (null !== $this->container) {
+                    // Replace the container entry name by the actual object
+                    $callable[0] = $this->container->get($callable[0]);
+                }
+
+                if (\is_string($callable[0]) && \class_exists($callable[0])) {
+                    $callable[0] = (new ReflectionClass($callable[0]))->newInstance();
+                }
+
+                return $callable;
+            } catch (Throwable $e) {
+                if ($this->container->has($callable[0])) {
+                    throw $e;
+                }
+
+                if ($e instanceof NotFoundExceptionInterface) {
+                    if ($isStaticCallToNonStaticMethod) {
+                        throw new NotCallableException(\sprintf(
+                            'Cannot call %s::%s() because %2$s() is not a static method and "%1$s" is not a container entry',
+                            $callable[0],
+                            $callable[1]
+                        ));
+                    }
+
+                    throw new NotCallableException(\sprintf(
+                        'Cannot call %s on %s because it is not a class nor a valid container entry',
+                        $callable[1],
+                        $callable[0]
+                    ));
+                }
+
+                throw NotCallableException::fromInvalidCallable($callable);
+            }
+        }
+
+        // Unrecognized stuff, we let it fail later
+        return $callable;
+    }
+
+    /**
+     * Check if the callable represents a static call to a non-static method.
+     *
+     * @param mixed $callable
+     *
+     * @return bool
+     */
+    private function isStaticCallToNonStaticMethod($callable)
+    {
+        if (\is_array($callable) && \is_string($callable[0])) {
+            list($class, $method) = $callable;
+            $reflection           = new ReflectionMethod($class, $method);
+
+            return !$reflection->isStatic();
+        }
+
+        return false;
+    }
+}
---
+++ tests/CallableResolverTest.php
@@ -0,0 +1,248 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP Invoker.
+ *
+ * PHP version 7.1 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DivineNii\Invoker\Tests;
+
+use DivineNii\Invoker\CallableResolver;
+use DivineNii\Invoker\Exceptions\InvocationException;
+use DivineNii\Invoker\Exceptions\NotCallableException;
+use DivineNii\Invoker\Tests\Fixtures\BlankClassMagic;
+use Exception;
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Throwable;
+
+/**
+ * CallableResolverTest
+ */
+class CallableResolverTest extends TestCase
+{
+    public function testConstructor(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $factory   = new CallableResolver($container);
+
+        $this->assertInstanceOf(CallableResolver::class, $factory);
+    }
+
+    public function testNotResolveWithStatic(): void
+    {
+        $factory = new CallableResolver();
+
+        $this->expectExceptionMessage(
+            '\'DivineNii\\\\Invoker\\\\Tests\\\\Fixtures\\\\BlankClass::staticNotMethod\'' .
+            ' is neither a callable nor a valid container entry'
+        );
+        $this->expectException(NotCallableException::class);
+
+        $factory->resolve('DivineNii\Invoker\Tests\Fixtures\BlankClass::staticNotMethod');
+    }
+
+    public function testNotResolveWithString(): void
+    {
+        $factory = new CallableResolver();
+
+        $this->expectExceptionMessage('\'handler\' is neither a callable nor a valid container entry');
+        $this->expectException(NotCallableException::class);
+
+        $factory->resolve('handler');
+    }
+
+    public function testNotResolveWithArray(): void
+    {
+        $factory = new CallableResolver();
+
+        $this->expectExceptionMessage('DivineNii\Invoker\Tests\Fixtures\BlankClass::none() is not a callable.');
+        $this->expectException(NotCallableException::class);
+
+        $factory->resolve([Fixtures\BlankClass::class, 'none']);
+    }
+
+    public function testResolveWithContainer(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('has')->with('handler')->willReturn(true);
+        $container->method('get')->willReturn(new Fixtures\BlankClass());
+
+        $factory = new CallableResolver($container);
+
+        $this->assertIsCallable($factory->resolve('handler'));
+        $this->assertIsCallable($factory->resolve(['handler', 'method']));
+    }
+
+    /**
+     * @dataProvider implicitContainerGets()
+     *
+     * @param mixed $unResolved
+     */
+    public function testResolveWithContainerHasAndException($unResolved): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('has')->with('handler')->willReturn(true);
+        $container->method('get')->willThrowException($this->notFoundException());
+
+        $this->expectExceptionMessage('Is not a callable, yeah.');
+        $this->expectException(NotFoundExceptionInterface::class);
+
+        $factory = new CallableResolver($container);
+        $factory->resolve($unResolved);
+    }
+
+    /**
+     * @dataProvider implicitContainerGets()
+     *
+     * @param mixed $unResolved
+     */
+    public function testResolveWithContainerHasNotAndException($unResolved): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('has')->with('handler')->willReturn(false);
+        $container->method('get')->willThrowException($this->notFoundException());
+
+        if (\is_array($unResolved)) {
+            $this->expectExceptionMessage(
+                'Cannot call method on handler because it is not a class nor a valid container entry'
+            );
+        } else {
+            $this->expectExceptionMessage('\'handler\' is neither a callable nor a valid container entry');
+        }
+        $this->expectException(NotCallableException::class);
+
+        $factory = new CallableResolver($container);
+        $factory->resolve($unResolved);
+    }
+
+    public function testResolveWithContainerHasNotAndStaticException(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('has')->with(Fixtures\BlankClassMagic::class)->willReturn(false);
+        $container->method('get')->willThrowException($this->notFoundException());
+
+        $this->expectExceptionMessage(
+            'Cannot call DivineNii\Invoker\Tests\Fixtures\BlankClassMagic::staticMethod() because staticMethod() ' .
+            'is not a static method and "DivineNii\Invoker\Tests\Fixtures\BlankClassMagic" is not a container entry'
+        );
+        $this->expectException(NotCallableException::class);
+
+        $factory = new CallableResolver($container);
+        $factory->resolve([BlankClassMagic::class, 'staticMethod']);
+    }
+
+    public function testResolveWithContainerHasNotAndMagicException(): void
+    {
+        $container    = $this->createMock(ContainerInterface::class);
+        $newException = new class ('is not a callable, none.') extends InvocationException {
+        };
+
+        $container->method('has')->with(Fixtures\BlankClassMagic::class)->willReturn(false);
+        $container->method('get')->willThrowException($newException);
+
+        $this->expectExceptionMessage(
+            'DivineNii\Invoker\Tests\Fixtures\BlankClassMagic::staticMethod() is not a callable. ' .
+            'A __call() method exists but magic methods are not supported.'
+        );
+        $this->expectException(NotCallableException::class);
+
+        $factory = new CallableResolver($container);
+        $factory->resolve([BlankClassMagic::class, 'staticMethod']);
+    }
+
+    /**
+     * @dataProvider implicitTypes
+     *
+     * @param mixed $unResolved
+     */
+    public function testResolve($unResolved): void
+    {
+        $factory = new CallableResolver();
+
+        $this->assertIsCallable($factory->resolve($unResolved));
+    }
+
+    /**
+     * @return Generator
+     */
+    public function implicitContainerGets(): Generator
+    {
+        yield 'String Invocable Get Type:' => [
+            'handler',
+        ];
+
+        yield 'String Method Get Type:' => [
+            ['handler', 'method'],
+        ];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function implicitTypes(): Generator
+    {
+        yield 'String Invocable Class Type:' => [
+            Fixtures\BlankClass::class,
+        ];
+
+        yield 'Object Invocable Class Type:' => [
+            new Fixtures\BlankClass(),
+        ];
+
+        yield 'Callable Class Type:' => [
+            [new Fixtures\BlankClass(), 'method'],
+        ];
+
+        yield 'Array Class Type:' => [
+            [Fixtures\BlankClass::class, 'method'],
+        ];
+
+        yield 'Pattern : Class Type:' => [
+            'DivineNii\Invoker\Tests\Fixtures\BlankClass:method',
+        ];
+
+        yield 'Pattern @ Class Type:' => [
+            'DivineNii\Invoker\Tests\Fixtures\BlankClass@method',
+        ];
+
+        yield 'Callable String Type:' => [
+            'phpinfo',
+        ];
+
+        yield 'Callable Closure Type:' => [
+            function (string $something): string {
+                return $something;
+            },
+        ];
+
+        yield 'Callable Static String Class Type 1' => [
+            'DivineNii\Invoker\Tests\Fixtures\BlankClass::staticMethod',
+        ];
+
+        yield 'Callable Static Array Class Type 1' => [
+            [Fixtures\BlankClass::class, 'staticMethod'],
+        ];
+    }
+
+    /**
+     * @return NotFoundExceptionInterface|Throwable
+     */
+    private function notFoundException()
+    {
+        return new class ('Is not a callable, yeah.') extends Exception implements NotFoundExceptionInterface {
+        };
+    }
+}
